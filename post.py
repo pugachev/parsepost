c5bc5d2a65e2de7c5c0062fc0f6caa917751046f
池フクロウ <mtaketani37@gmail.com>
2022-03-22 17:43:22 +0900

    CSVから読み込んでアップロードする方法に修正

--- post.py
+++ post.py
@@ -4,6 +4,7 @@ from urllib.parse import urljoin
 import time
 import glob
 import os
+import csv
 
 # メールアドレスとパスワードの指定
 USER = "mtake"
@@ -27,19 +28,22 @@ url_login = "https://blog.ikefukuro40.tech/login.php"
 res = session.post(url_login, data=login_info)
 res.raise_for_status() # エラーならここで例外を発生させる
 
-list = glob.glob('./tgtphtos/*')
-for tgtfile in list:
-    # file = [('image', ('takada.jpg', open('takada.jpg', 'rb'), 'image/jpeg'))]
-    print(os.path.split(tgtfile)[1])
-    file = [('image', (tgtfile, open(tgtfile, 'rb'), 'image/jpeg'))]
+csv_file = open("./test_stock.csv", "r", encoding="utf-8", errors="", newline="" )
+f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
+header = next(f)
+for row in f:
+
+    tgtpic='./tgtphtos/'+row[2]
+
+    file = [('image', (tgtpic, open(tgtpic, 'rb'), 'image/jpeg'))]
 
     headers = { "Content-Type": "multipart/form-data" }
     request_data = {
         "name":USER,
         "password":PASS,
-        "title":"連続美人さん",
-        "body":"美人さんを連続POSTしています",
-        "category":"1",
+        "title":row[0],
+        "body":row[1],
+        "category":row[3],
         "headers":headers,
         "back":"https://blog.ikefukuro40.tech/backend.php",
         "mml_id":"0"
@@ -50,6 +54,34 @@ for tgtfile in list:
     res = session.post(url_post, files=file, data=request_data)
     print(res)
     time.sleep(5)
+
+
+
+
+
+# list = glob.glob('./tgtphtos/*')
+# for tgtfile in list:
+#     # file = [('image', ('takada.jpg', open('takada.jpg', 'rb'), 'image/jpeg'))]
+#     print(os.path.split(tgtfile)[1])
+#     file = [('image', (tgtfile, open(tgtfile, 'rb'), 'image/jpeg'))]
+
+#     headers = { "Content-Type": "multipart/form-data" }
+#     request_data = {
+#         "name":USER,
+#         "password":PASS,
+#         "title":"連続美人さん",
+#         "body":"美人さんを連続POSTしています",
+#         "category":"1",
+#         "headers":headers,
+#         "back":"https://blog.ikefukuro40.tech/backend.php",
+#         "mml_id":"0"
+#     }
+
+#     url_post = 'https://blog.ikefukuro40.tech/post.php'
+
+#     res = session.post(url_post, files=file, data=request_data)
+#     print(res)
+#     time.sleep(5)
 
 
  
