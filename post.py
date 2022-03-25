673d2ee033815581c352aeaebe9b880551f8c6f3
池フクロウ <mtaketani37@gmail.com>
2022-03-25 22:56:57 +0900

    いよいよDayOneアップデート開始

--- post.py
+++ post.py
@@ -10,6 +10,7 @@ import csv
 USER = "mtake"
 PASS = "Manabu2010"
 OPTION="python"
+preUUID=''
 
 # セッションを開始
 session = requests.session()
@@ -28,32 +29,39 @@ url_login = "https://blog.ikefukuro40.tech/login.php"
 res = session.post(url_login, data=login_info)
 res.raise_for_status() # エラーならここで例外を発生させる
 
-csv_file = open("./test_stock.csv", "r", encoding="utf-8", errors="", newline="" )
+# csv_file = open("./test_stock.csv", "r", encoding="utf-8", errors="", newline="" )
+csv_file = open("./Journal.csv", "r", encoding="utf-8", errors="", newline="" )
 f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
 header = next(f)
 for row in f:
 
-    tgtpic='./tgtphtos/'+row[2]
+    if row[2]!=preUUID:
+        preUUID=row[2]
+    
+        # tgtpic='./tgtphtos/'+row[2]
+        if len(row[45])!=0:
+            tgtpic='./photos/'+row[45]+'.jpeg'
+            file = [('image', (tgtpic, open(tgtpic, 'rb'), 'image/jpeg'))]
 
-    file = [('image', (tgtpic, open(tgtpic, 'rb'), 'image/jpeg'))]
+        dtitle=row[0]
+        
+        headers = { "Content-Type": "multipart/form-data" }
+        request_data = {
+            "name":USER,
+            "password":PASS,
+            "title":dtitle[0:10],
+            "body":row[1],
+            "category":row[3],
+            "headers":headers,
+            "back":"https://blog.ikefukuro40.tech/backend.php",
+            "mml_id":"0"
+        }
 
-    headers = { "Content-Type": "multipart/form-data" }
-    request_data = {
-        "name":USER,
-        "password":PASS,
-        "title":row[0],
-        "body":row[1],
-        "category":row[3],
-        "headers":headers,
-        "back":"https://blog.ikefukuro40.tech/backend.php",
-        "mml_id":"0"
-    }
+        url_post = 'https://blog.ikefukuro40.tech/post.php'
 
-    url_post = 'https://blog.ikefukuro40.tech/post.php'
-
-    res = session.post(url_post, files=file, data=request_data)
-    print(res)
-    time.sleep(5)
+        res = session.post(url_post, files=file, data=request_data)
+        print(res)
+        time.sleep(5)
 
 
 
--- readcsv.py
+++ readcsv.py
@@ -1,11 +1,18 @@
 import csv
 
-csv_file = open("./test_stock.csv", "r", encoding="utf-8", errors="", newline="" )
+csv_file = open("Journal.csv", "r", encoding="utf-8", errors="", newline="" )
 
 f = csv.reader(csv_file, delimiter=",", doublequote=True, lineterminator="\r\n", quotechar='"', skipinitialspace=True)
 header = next(f)
-print(header)
+#print(header)
+preUUID=''
 for row in f:
     #rowはList
     #row[0]で必要な項目を取得することができる
-    print(row[1])
+    #uuidが同じ値が2つ連続する仕様なので、2つ目を取るとする
+    #ある値と次の値を比較する 今回分を前回分としてとっておくpreValueとしましょうかね
+    if row[2]==preUUID:
+        print("")
+    else:
+        print(row[2])
+        preUUID=row[2]
